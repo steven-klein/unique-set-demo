5c028f0d56db7541165e7894ff7112aaccd430be
Steven Klein <steven.p.klein@gmail.com>
2017-05-23 10:29:17 -0500

    linting

--- js/uniqueSet/src/exponential.js
+++ js/uniqueSet/src/exponential.js
@@ -1,10 +1,13 @@
-export const exponential = function (list) {
-    for(let i = 0; i < list.length; i++){
-        for(let j = (i + 1); j < list.length; j++) {
-            if(list[i] == list[j])
-                list.splice(j, 1);
-        }
+function exponential (list) {
+  for (let i = 0; i < list.length; i++) {
+    for (let j = (i + 1); j < list.length; j++) {
+      if (list[i] === list[j]) {
+        list.splice(j, 1)
+      }
     }
+  }
 
-    return list;
+  return list
 }
+
+module.exports = exponential
--- js/uniqueSet/src/recursive.js
+++ js/uniqueSet/src/recursive.js
@@ -1,13 +1,16 @@
-export const recursive = function (list, map, filtered) {
-    if(list.length == 0)
-        return filtered;
+function recursive (list, map, filtered) {
+  if (list.length === 0) {
+    return filtered
+  }
 
-    if(!map.hasOwnProperty(list[0])) {
-        map[list[0]] = true;
-        filtered.push(list[0]);
-    }
+  if (!map.hasOwnProperty(list[0])) {
+    map[list[0]] = true
+    filtered.push(list[0])
+  }
 
-    list.shift();
+  list.shift()
 
-    return filterRecursive(list, map, filtered);
+  return recursive(list, map, filtered)
 }
+
+module.exports = recursive
